8fe0572883bd988263c15216475cc41d2b028de8
PoojaChirp <pshashidhar@twitter.com>
2018-01-22 10:18:38 -0500

    Aol adapters added

--- Aol/MoPub-Aol-Adapters/Example/Pods/Target Support Files/MoPub-Aol-Adapters/MoPub-Aol-Adapters-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MoPub_Aol_Adapters : NSObject
-@end
-@implementation PodsDummy_MoPub_Aol_Adapters
-@end
--- Aol/MoPub-Aol-Adapters/Example/Pods/Target Support Files/Pods-MoPub-Aol-Adapters_Tests/Pods-MoPub-Aol-Adapters_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MoPub_Aol_Adapters_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_MoPub_Aol_Adapters_Tests
-@end
