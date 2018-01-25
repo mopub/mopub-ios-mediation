748ffbf73e3302b229dd5e6e96ec98f8b0fc9c41
PoojaChirp <pshashidhar@twitter.com>
2018-01-25 15:25:32 -0500

    adding cocoapods push script

---
+++ push.sh
@@ -0,0 +1,4 @@
+COMMAND='pod spec lint --allow-warnings --use-libraries --verbose'
+find `pwd` -iname "*-PodSpecs"  | sort -u | while read i; do                                              
+    cd "$i" && pwd && $COMMAND
+done
