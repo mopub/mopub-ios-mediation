0676c9ed6b03f8dca6a895e00a184bee84e0c7f4
PoojaChirp <pshashidhar@twitter.com>
2018-02-14 14:28:26 -0500

    Changed files

--- push.sh
+++
@@ -1,7 +0,0 @@
-COMMAND='pod spec lint --allow-warnings --use-libraries --verbose'
-COMMAND2='pod trunk push --allow-warnings --use-libraries --verbose'
-find `pwd` -iname "*-PodSpecs"  | sort -u | while read i; do
-cd "$i" && pwd && $COMMAND && $COMMAND2
-done
-
-#remove # sign when required to push
