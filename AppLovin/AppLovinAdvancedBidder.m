fdf5379112afcd28f2de1648725556e5fa5c72bb
Kelly Dun <kdun@twitter.com>
2018-09-17 14:56:16 -0700

    Added SDK key check to AppLovin token so it will not crash if no token is available.

--- AppLovin/AppLovinAdvancedBidder.m
+++ AppLovin/AppLovinAdvancedBidder.m
@@ -23,6 +23,12 @@
 
 - (NSString *)token
 {
+    // Check if the SDK key is present in info.plist. If it's not present, do not attempt
+    // to fetch the token since this will cause the app to crash!
+    if ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"AppLovinSdkKey"] == nil) {
+        return nil;
+    }
+    
     return [ALSdk shared].adService.bidToken;
 }
 
