4d8376c28af8ba04981e0a31eae26fe0ac4b5b1f
maninomula <nomulam@google.com>
2018-09-21 13:43:01 -0700

    Updating AdMob Adapter to use Unified native Ad and remove deprecated adTypes.

--- AdMob/MPGoogleAdMobBannerCustomEvent.h
+++ AdMob/MPGoogleAdMobBannerCustomEvent.h
@@ -6,12 +6,12 @@
 //
 
 #if __has_include(<MoPub/MoPub.h>)
-    #import <MoPub/MoPub.h>
+#import <MoPub/MoPub.h>
 #elif __has_include(<MoPubSDKFramework/MoPub.h>)
-    #import <MoPubSDKFramework/MoPub.h>
+#import <MoPubSDKFramework/MoPub.h>
 #else
-    #import "MPBannerCustomEvent.h"
-    #import "MoPub.h"
+#import "MPBannerCustomEvent.h"
+#import "MoPub.h"
 #endif
 
 #import "MPGoogleGlobalMediationSettings.h"
