cb979c1a8b43ad125e9ce6e9dc395c77695fec3a
Fritz Huie <fritzhuie@users.noreply.github.com>
2019-01-28 13:23:32 -0800

    Update MPUnityRouter.m

--- UnityAds/MPUnityRouter.m
+++ UnityAds/MPUnityRouter.m
@@ -85,13 +85,11 @@
 - (void)requestVideoAdWithGameId:(NSString *)gameId placementId:(NSString *)placementId delegate:(id<MPUnityRouterDelegate>)delegate;
 {
     [self updateConsentStatus];
-       
     if([UnityAds getPlacementState:placementId] == kUnityAdsPlacementStateNoFill){
         NSError *error = [NSError errorWithDomain:MoPubRewardedVideoAdsSDKDomain code:MPRewardedVideoAdErrorUnknown userInfo:nil];
         [delegate unityAdsDidFailWithError:error];
         return;
     }
-    
     if (!self.isAdPlaying) {
         [self.delegateMap setObject:delegate forKey:placementId];
         [self initializeWithGameId:gameId];
