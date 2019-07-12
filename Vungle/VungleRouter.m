ded77ef35d1d0ee113c912d23ae0ee698e030a12
YueVungle <yueting.cui@vungle.com>
2019-07-12 14:44:34 +0800

    Update Adapter version to 6.4.2.0

--- Vungle/VungleRouter.m
+++ Vungle/VungleRouter.m
@@ -14,7 +14,7 @@
 #endif
 #import "VungleInstanceMediationSettings.h"
 
-static NSString *const VungleAdapterVersion = @"6.4.1";
+static NSString *const VungleAdapterVersion = @"6.4.2.0";
 
 NSString *const kVungleAppIdKey = @"appId";
 NSString *const kVunglePlacementIdKey = @"pid";
