5b38986a95559e7e77275361f2c31c4ef277e874
YueVungle <yueting.cui@vungle.com>
2019-09-26 11:10:02 +0800

    Support 6.4.5 SDK

--- Vungle/VungleRouter.m
+++ Vungle/VungleRouter.m
@@ -14,7 +14,7 @@
 #endif
 #import "VungleInstanceMediationSettings.h"
 
-static NSString *const VungleAdapterVersion = @"6.4.4.0";
+static NSString *const VungleAdapterVersion = @"6.4.5.0";
 
 NSString *const kVungleAppIdKey = @"appId";
 NSString *const kVunglePlacementIdKey = @"pid";
