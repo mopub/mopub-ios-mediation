1bb6d4c9c72a8af07274bb870b35e5f4b6e035a9
Felix Zhang <felix.zhang@vungle.com>
2020-04-01 14:30:33 +0800

    add currentSuperToken api

--- Vungle/VungleRouter.h
+++ Vungle/VungleRouter.h
@@ -40,6 +40,7 @@ extern const CGSize kVNGLeaderboardBannerSize;
 - (void)requestRewardedVideoAdWithCustomEventInfo:(NSDictionary *)info delegate:(id<VungleRouterDelegate>)delegate;
 - (void)requestBannerAdWithCustomEventInfo:(NSDictionary *)info size:(CGSize)size delegate:(id<VungleRouterDelegate>)delegate;
 - (BOOL)isAdAvailableForPlacementId:(NSString *)placementId;
+- (NSString *)currentSuperToken;
 - (void)presentInterstitialAdFromViewController:(UIViewController *)viewController options:(NSDictionary *)options forPlacementId:(NSString *)placementId;
 - (void)presentRewardedVideoAdFromViewController:(UIViewController *)viewController customerId:(NSString *)customerId settings:(VungleInstanceMediationSettings *)settings forPlacementId:(NSString *)placementId;
 - (UIView *)renderBannerAdInView:(UIView *)bannerView options:(NSDictionary *)options forPlacementID:(NSString *)placementID size:(CGSize)size;
