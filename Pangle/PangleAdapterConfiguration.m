efba4683ed0b658cf3b32714931a27d217fd16e2
wangyanlin.tech@bytedance.com <wangyanlin.tech@bytedance.com>
2020-06-11 21:33:25 +0800

    configuration update

--- Pangle/PangleAdapterConfiguration.m
+++ Pangle/PangleAdapterConfiguration.m
@@ -6,7 +6,7 @@
 NSString * const kPangleAppIdKey = @"app_id";
 NSString * const kPanglePlacementIdKey = @"ad_placement_id";
 
-static NSString * const kAdapterVersion = @"3.0.0.6.1";
+static NSString * const kAdapterVersion = @"3.0.0.7.1";
 static NSString * const kAdapterErrorDomain = @"com.mopub.mopub-ios-sdk.mopub-pangle-adapters";
 
 typedef NS_ENUM(NSInteger, PangleAdapterErrorCode) {
@@ -28,7 +28,7 @@ typedef NS_ENUM(NSInteger, PangleAdapterErrorCode) {
 }
 
 - (NSString *)networkSdkVersion {
-    return [BUAdSDKManager SDKVersion]?:@"";
+    return [BUAdSDKManager SDKVersion];
 }
 
 - (void)initializeNetworkWithConfiguration:(NSDictionary<NSString *, id> *)configuration complete:(void(^)(NSError *))complete {
@@ -37,32 +37,37 @@ typedef NS_ENUM(NSInteger, PangleAdapterErrorCode) {
         NSError *error = [NSError errorWithDomain:kAdapterErrorDomain
                                              code:PangleAdapterErrorCodeMissingIdKey
                                          userInfo:@{NSLocalizedDescriptionKey:
-                                                        @"appId may be not right, please set networkConfig refer to method '-configCustomEvent' in 'AppDelegate' class"}];
+                                                        @"Invalid or missing Pangle appId, please set networkConfig refer to method '-configCustomEvent' in 'AppDelegate' class"}];
         if (complete != nil) {
             complete(error);
         }
     } else {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [BUAdSDKManager setAppID:appId];
-            MPBLogLevel logLevel = [MPLogging consoleLogLevel];
-            BOOL verboseLoggingEnabled = (logLevel == MPBLogLevelDebug);
-            [BUAdSDKManager setLoglevel:verboseLoggingEnabled == true ? BUAdSDKLogLevelDebug : BUAdSDKLogLevelNone];
-            if ([[MoPub sharedInstance] isGDPRApplicable] != MPBoolUnknown) {
-                BOOL canCollect =  [[MoPub sharedInstance] canCollectPersonalInfo];
-                /// Custom set the GDPR of the user,GDPR is the short of General Data Protection Regulation,the interface only works in The European.
-                /// @params GDPR 0 close privacy protection, 1 open privacy protection
-                [BUAdSDKManager setGDPR:canCollect ? 0 : 1];
-            }
-            if (complete != nil) {
-                complete(nil);
-            }
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [BUAdSDKManager setAppID:appId];
+                MPBLogLevel logLevel = [MPLogging consoleLogLevel];
+                BOOL verboseLoggingEnabled = (logLevel == MPBLogLevelDebug);
+                
+                [BUAdSDKManager setLoglevel:(verboseLoggingEnabled == true ? BUAdSDKLogLevelDebug : BUAdSDKLogLevelNone)];
+                if ([[MoPub sharedInstance] isGDPRApplicable] != MPBoolUnknown) {
+                    BOOL canCollectPersonalInfo =  [[MoPub sharedInstance] canCollectPersonalInfo];
+                    /// Custom set the GDPR of the user,GDPR is the short of General Data Protection Regulation,the interface only works in The European.
+                    /// @params GDPR 0 close privacy protection, 1 open privacy protection
+                    [BUAdSDKManager setGDPR:canCollectPersonalInfo ? 0 : 1];
+                }
+                if (complete != nil) {
+                    complete(nil);
+                }
+            });
         });
     }
 }
 
-#pragma mark - Caching
+#pragma mark - Update the network initialization parameters cache
 + (void)updateInitializationParameters:(NSDictionary *)parameters {
     NSString * appId = parameters[kPangleAppIdKey];
+    
     if (BUCheckValidString(appId)) {
         NSDictionary * configuration = @{kPangleAppIdKey: appId};
         [PangleAdapterConfiguration setCachedInitializationParameters:configuration];
