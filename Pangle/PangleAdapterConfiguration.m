744150d7d3480522357ed456328e72d0433bda3b
wangyanlin.tech@bytedance.com <wangyanlin.tech@bytedance.com>
2020-06-11 21:52:03 +0800

    添加         static dispatch_once_t onceToken;

--- Pangle/PangleAdapterConfiguration.m
+++ Pangle/PangleAdapterConfiguration.m
@@ -42,21 +42,24 @@ typedef NS_ENUM(NSInteger, PangleAdapterErrorCode) {
             complete(error);
         }
     } else {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [BUAdSDKManager setAppID:appId];
-            MPBLogLevel logLevel = [MPLogging consoleLogLevel];
-            BOOL verboseLoggingEnabled = (logLevel == MPBLogLevelDebug);
-            
-            [BUAdSDKManager setLoglevel:(verboseLoggingEnabled == true ? BUAdSDKLogLevelDebug : BUAdSDKLogLevelNone)];
-            if ([[MoPub sharedInstance] isGDPRApplicable] != MPBoolUnknown) {
-                BOOL canCollectPersonalInfo =  [[MoPub sharedInstance] canCollectPersonalInfo];
-                /// Custom set the GDPR of the user,GDPR is the short of General Data Protection Regulation,the interface only works in The European.
-                /// @params GDPR 0 close privacy protection, 1 open privacy protection
-                [BUAdSDKManager setGDPR:canCollectPersonalInfo ? 0 : 1];
-            }
-            if (complete != nil) {
-                complete(nil);
-            }
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [BUAdSDKManager setAppID:appId];
+                MPBLogLevel logLevel = [MPLogging consoleLogLevel];
+                BOOL verboseLoggingEnabled = (logLevel == MPBLogLevelDebug);
+                
+                [BUAdSDKManager setLoglevel:(verboseLoggingEnabled == true ? BUAdSDKLogLevelDebug : BUAdSDKLogLevelNone)];
+                if ([[MoPub sharedInstance] isGDPRApplicable] != MPBoolUnknown) {
+                    BOOL canCollectPersonalInfo =  [[MoPub sharedInstance] canCollectPersonalInfo];
+                    /// Custom set the GDPR of the user,GDPR is the short of General Data Protection Regulation,the interface only works in The European.
+                    /// @params GDPR 0 close privacy protection, 1 open privacy protection
+                    [BUAdSDKManager setGDPR:canCollectPersonalInfo ? 0 : 1];
+                }
+                if (complete != nil) {
+                    complete(nil);
+                }
+            });
         });
     }
 }
