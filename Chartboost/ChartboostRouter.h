cd7d8feb85dfead75b601000129836d51040344c
Arnau Marti <arnau.marti@chartboost.com>
2020-10-08 09:43:30 +0200

    added macro for mopup

--- Chartboost/ChartboostRouter.h
+++ Chartboost/ChartboostRouter.h
@@ -21,6 +21,8 @@
     #import "Chartboost+Mediation.h"
 #endif
 
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ChartboostRouter : NSObject
