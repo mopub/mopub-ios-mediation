54eaaf28a969fc1aa3001a86245e363c2e914c7a
Akshit Garg <akshit.garg@inmobi.com>
2020-12-04 15:07:17 +0530

    InMobi MoPub plugin Initial commit

---
+++ InMobi/InMobiInterstitialAd.h
@@ -0,0 +1,26 @@
+//
+//  InMobiInterstitialAd.h
+//  InMobiMopubAdvancedBiddingPlugin
+//  InMobi
+//
+//  Created by Akshit Garg on 24/11/20.
+//  Copyright © 2020 InMobi. All rights reserved.
+//
+
+#if __has_include(<MoPub/MoPub.h>)
+    #import <MoPub/MoPub.h>
+#elif __has_include(<MoPubSDKFramework/MoPub.h>)
+    #import <MoPubSDKFramework/MoPub.h>
+#else
+    #import "MPFullscreenAdAdapter.h"
+#endif
+
+#import <InMobiSDK/IMInterstitial.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InMobiInterstitialAd : MPFullscreenAdAdapter <MPThirdPartyFullscreenAdAdapter, IMInterstitialDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ InMobi/README.md
@@ -0,0 +1,7 @@
+## Overview
+  * This folder contains mediation adapters used to mediate Inmobi.
+  * To download and integrate the Inmobi SDK, please check (https://www.inmobi.com/sdk).
+  * For inquiries and support, please [email](https://support.inmobi.com).
+  
+## Adapter integration
+  * To integrate adapters, please visit our [integration tutorial](https://support.inmobi.com/monetize/mediation-guidelines/mopub/ios-mopub-guidelines).
